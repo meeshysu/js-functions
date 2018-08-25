d412fda04547c03aa42ef448420be8945be77e65
Michelle Beshears <meeshysu@gmail.com>
2018-08-25 10:18:36 -0500

    let, const, template literals'

---
+++ main.js
@@ -0,0 +1,17 @@
+const bananaDiv = document.getElementById('banana');
+bananaDiv.innerHTML = 'i am a banana';
+
+let counter = 0;
+counter += 1; 
+counter++
+
+// counter = 3 because every single line has run. you started with zero and then added and then added.
+console.log(counter);
+console.log('counter: ', counter)
+// having the word of the variable name 
+
+const greetingElement = document.getElementById('greeting');
+const firstName = 'Michelle';
+const lastName = 'Beshears';
+     greetingElement.innerHTML = `Hello my name is ${firstName} ${lastName}`;
+
