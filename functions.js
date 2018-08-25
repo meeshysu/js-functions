1ac19daebeb6ed0a106ce840b3758d2f49c30467
Michelle Beshears <meeshysu@gmail.com>
2018-08-25 11:48:33 -0500

    made writeToDom

--- functions.js
+++ functions.js
@@ -22,3 +22,21 @@ const nomnom = (person, food) => {
 
 const bearSticks = nuggetizer2('bear');
 console.log(nomnom('zoe', bearSticks)); // zoe devoured bear stix
+
+//function numberAdder that takes in a number and 
+//returns the number plus 3. so input=22, output =25.
+
+const numberAdder = (num) => {
+    //return num +3; below we are using that variable to print to the dom instead.
+    const finalNumber = num + 3;
+    printToDom(`<h2>${finalNumber}</h2>`, 'allTheNumbers');
+};
+//this code you will be using quite frequently
+const printToDom = (stringToPrint, divId) => {
+    const selectedDiv = document.getElementById(divId);
+    selectedDiv.innerHTML += stringToPrint;
+};
+//console.log('num: ', numberAdder(22)); again printing to the dom below 
+numberAdder(22);
+numberAdder(2);
+
